7737ed699acb397f6ddb58b718bb32e212076213
OsabuohienAisosaJonathan <aisosa.osabuohien@eng.uniben.edu>
2026-02-10 17:49:28 +0100

    new real fix

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -14,6 +14,7 @@ export interface User {
   phone?: string | null;
   mustChangePassword?: boolean;
   organizationId?: string | null;
+  sessionToken?: string;
 }
 
 export interface AdminActivityLog {
